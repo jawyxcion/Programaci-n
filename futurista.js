00c758f3bd033e543fb1d7a76796cabe37164ad8
jawyxcion <shamiruzziel27@gmail.com>
2024-10-23 16:39:30 -0600

    si

--- futurista.js
+++ futurista.js
@@ -5,3 +5,30 @@ document.querySelectorAll('.navbar-item').forEach(item => {
       document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.side-navbar');
+    const content = document.querySelector('.content');
+    const contactBar = document.querySelector('.contact-bar');
+
+    // Ajustar posición del contenido basado en el ancho del navbar
+    navbar.addEventListener('mouseenter', function() {
+        content.style.marginLeft = '250px';
+        contactBar.style.paddingLeft = '250px';
+    });
+
+    navbar.addEventListener('mouseleave', function() {
+        content.style.marginLeft = '100px';
+        contactBar.style.paddingLeft = '100px';
+    });
+});
+
+// Función para confirmar el cierre de sesión
+function confirmLogout() {
+  // Muestra un cuadro de confirmación
+  const confirmation = confirm("¿Estás seguro que deseas cerrar la sesión?");
+  if (confirmation) {
+      // Si el usuario acepta, redirige a index.html
+      window.location.href = 'index.html';
+    }
+}
